23e3c9054e595cc7f0a8c0c015e289f65e520dc6
tri-bui <buitri91@gmail.com>
2020-12-11 13:59:04 -0800

    Move city data to external js file

--- earthquake-mapper/map-multiple-points/static/js/logic.js
+++ earthquake-mapper/map-multiple-points/static/js/logic.js
@@ -11,52 +11,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: MAPBOX_KEY
 }).addTo(map);
 
-// Coordinates for the 5 most populated U.S. cities
-let cities = [
-    {
-        location: [40.7128, -74.0059],
-        city: "New York City",
-        state: "NY",
-        population: 8398748
-    },
-    {
-        location: [41.8781, -87.6298],
-        city: "Chicago",
-        state: "IL",
-        population: 2705994
-    },
-    {
-        location: [29.7604, -95.3698],
-        city: "Houston",
-        state: "TX",
-        population: 2325502
-    },
-    {
-        location: [34.0522, -118.2437],
-        city: "Los Angeles",
-        state: "CA",
-        population: 3990456
-    },
-    {
-        location: [33.4484, -112.0740],
-        city: "Phoenix",
-        state: "AZ",
-        population: 1660272
-    }
-];
+// Cities data
+let cityData = cities;
 
 // Add a marker for each city to the map
-cities.forEach(city => {
+cityData.forEach(city => {
     L.marker(city.location).addTo(map);
 });
-
-// // Add 25km-radius circle for LA to map
-// L.circle([34.0522, -118.2437], {
-//     radius: 25e3,
-//     color: 'red',
-//     fillColor: 'yellow',
-//     fillOpacity: 0.25
-// }).addTo(map);
-
-// // Add marker for LAX to map
-// L.marker([33.9416, -118.4085]).addTo(map);
