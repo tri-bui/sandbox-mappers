76a5ae2e0f08a9ecc54ba6a1ce6e7b8817ad4949
tri-bui <buitri91@gmail.com>
2021-02-27 13:24:20 -0800

    Refactor leaflet-mapper/map-geojson-lines/logic.js

--- leaflet-mapper/map-geojson-linestrings/static/js/logic.js
+++ leaflet-mapper/map-geojson-linestrings/static/js/logic.js
@@ -25,26 +25,28 @@ let baseMaps = {
 };
 
 
-// Map object with center of the Toronto and a zoom level of 2
-let map = L.map('route-map', {center: [44.0, -80.0], zoom: 2, layers: [light]});
+// Map object with center of the Toronto and a zoom level of 3
+let map = L.map('route-map', {center: [44.0, -80.0], zoom: 3, layers: [light]});
 
 // Add base layer to map
 L.control.layers(baseMaps).addTo(map);
 
 
 // Toronto routes geoJSON data
-let routes = 'https://raw.githubusercontent.com/tri-bui/sandbox-mappers/main/leaflet-practice/map-geojson-linestrings/static/js/torontoRoutes.json';
+let routes = 'https://raw.githubusercontent.com/tri-bui/sandbox-mappers/main/leaflet-mapper/map-geojson-linestrings/static/js/torontoRoutes.json';
+
+// Line styling
+let lineStyle = {color: 'yellow', weight: 2, opacity: 0.5};
 
 // Add Toronto routes to map
 d3.json(routes).then(data => {
-    L.geoJSON(data, {style: {
-        color: 'yellow',
-        weight: 2,
-        opacity: 0.5
-    }, onEachFeature: (feat, layer) => {
-        let props = feat.properties;
-        layer.bindPopup(
-            `<h2>${props.src} to ${props.dst} (${props.stops} stops)</h2>`
-        );
-    }}).addTo(map);
+    L.geoJSON(data, {
+        style: lineStyle, 
+        onEachFeature: (feat, layer) => {
+            let props = feat.properties;
+            layer.bindPopup(
+                `<h2>${props.src} to ${props.dst} (${props.stops} stops)</h2>`
+            );
+        }
+    }).addTo(map);
 });
